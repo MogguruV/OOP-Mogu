2280d82b5790d74769d49a6f615ed47c3e68a326
MogguruV <angga123843@gmail.com>
2024-11-14 14:56:10 +0800

    New

--- Main.java
+++ Main.java
@@ -1,65 +1,15 @@
-import Products.Electronics;
-import Products.Fashion;
+import UTS.Contract;
+import UTS.Permanent;
 
 public class Main {
      public static void main(String[] args) {
-      // ProviderTelecom provider = new ProviderTelecom();
-      // System.out.println("Hello Jawa");
-      // provider.getInfo("SarataKom", 10000, 1 );
-      // provider.tambahPulsa(1000);
-      // provider.kurangiPulsa(700);
-      // provider.tambahKuota(12);
-      // provider.kurangiKuota(5);
-      // provider.perpanjangMasaBerlakuPulsa();
 
-      //  Player player = new Player();
-      //  player.GetInfoPlayer();
-      //  player.moveRight();
-      //  player.moveLeft();
-      //  player.moveUp();
-      //  player.moveDown();
-      //  player.getDamage(2, 1);
-      //  player.getPower();
-      //  player.attack();
-      //  player.dash(5);
-      //  player.isDead();
-      //  player.isRespawn();
-      //  player.firstSkill(true);
-      //  player.secondSkill(true);
-      //  player.ultimateSkill(true);
+          Contract contract = new Contract(332111090, "Putri", "Sutradara", 100000000, 2000000, 4000000);
+          contract.getEmployee();
+          contract.getContractDuration("10 Tahun\n");
 
-      // Person person = new Person();
-      // person.setName("Mogu");
-      // person.setAge(111);
-      // person.getInfo();
 
-      // ProductBasket basket = new ProductBasket();
-      // basket.addProduct(3);
-      // basket.removeProduct(4);
-      // basket.getInfoProductBasket();
-
-      // LocalStorage localStorage = new LocalStorage();
-      // localStorage.setItem("abcd1234", "MogguruV");
-      // localStorage.getItem("abcd1234");
-
-     //QUIZZ
-
-     // Quis cookies = new Quis();
-     // cookies.setItems(1111, "Hatsune Miku", 10); //hari expired dalam hari
-     // cookies.getItems(1111);
-     // cookies.remove(0);
-     // cookies.expired(7); //batas waktu expired dalam hari
-
-     // Para mogu = new Para();
-     // mogu.perkenalan("joyi", 1000, "game"); 
-     // mogu.namaku();
-
-     Electronics elektronik = new Electronics("MoguMogu", "Adalah laptop tercanggih yang sudah support MAI (Miku AI)", 159000000, 3);
-     elektronik.getInfo();
-     elektronik.getInfoWarranty();
-
-     Fashion fashion = new Fashion("AdiMiku", "Sepatu Hatsune Miku Terbaru", 6900000, "XL");
-     fashion.getInfo();
-     fashion.getSize();
+          Permanent permanent = new Permanent(332106090, "Kobo", "V-Tuber", 1000000000, 1000000, 3000000);
+          permanent.getSalary();
      }
 }
--- UTS/Employee.java
+++ UTS/Employee.java
@@ -1,7 +1,8 @@
 package UTS;
 
 // Class
-public class Employee { 
+public class Employee {
+
     // Attributes
     private int nik, basic_salary, transport_allowance, position_allowance, takehomepay;
     private String name, position_name;
@@ -26,7 +27,7 @@ public class Employee {
         this.position_allowance = position_allowance;
         System.out.println("Berhasil menambahkan karyawan baru, " + name);
     }
-    
+
     // Method
     public void getEmployee(){
         System.out.println(name + " - " + nik + " menduduki posisi " + position_name);
@@ -35,6 +36,6 @@ public class Employee {
     // Method
     public void getSalary(){
         takehomepay = basic_salary + transport_allowance + position_allowance;
-        System.out.println(name + " | " + position_name + " mendapatkan total gaji, +" + takehomepay + " Rupiah");
+        System.out.println(name + " | " + position_name + " mendapatkan total gaji, " + takehomepay + " Rupiah");
     }
 }
