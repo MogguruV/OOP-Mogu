7a367e9715d666eef11d842a21c92668ab4c6ca3
MogguruV <angga123843@gmail.com>
2024-11-20 20:27:51 +0800

    new

--- Belajar.java
+++ Belajar.java
@@ -1,16 +1,3 @@
 public class Belajar {  
-    int P;                          //Attribute panjang persegi P
-    int l;                          //Attrubute lebear persegi P
-    int L;                                 //Attribute Luas persegi P
-
-
-    public Belajar(int p, int l){
-        this.P = p;
-        this.l = l;
-    }
     
-    public void luasPersegiPanjang(){
-        L = P * l ;
-        System.out.println("Nickname : Joyi \nGame : Roblox");
-    }
 }
