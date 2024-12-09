1ac0b84e27f84e9cb52920dda28e0648bde7dad5
MogguruV <angga123843@gmail.com>
2024-12-09 17:32:30 +0800

    new

--- Belajar.java
+++ Belajar.java
@@ -3,9 +3,7 @@ public class Belajar {
     int[] array = {1,2,3,4,5,6,7,8,9,0};
     int angka = 1;
 
-    public void printArray(){
-        for (int angka : array){
-            System.out.println(angka);
-        }
-    }
+    
+
+    
 }
